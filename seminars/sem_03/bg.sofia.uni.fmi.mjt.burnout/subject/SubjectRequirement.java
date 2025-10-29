2272aa91667af8b35ce29e3f121126ae88704a5b
Irena Stancheva <147410058+ren-rens@users.noreply.github.com>
2025-10-29 15:25:07 +0200

    Create SubjectRequirement.java

---
+++ seminars/sem_03/bg.sofia.uni.fmi.mjt.burnout/subject/SubjectRequirement.java
@@ -0,0 +1,19 @@
+package bg.sofia.uni.fmi.mjt.burnout.subject;
+/**
+ *
+ * @param category the academic category this subject belongs to
+ * @param minAmountEnrolled minimum amount of subject enrolled for the category
+ *
+ * @throws IllegalArgumentException if the category is null
+ * @throws IllegalArgumentException if the credits are negative
+ */
+public record SubjectRequirement(Category category, int minAmountEnrolled) {
+    public SubjectRequirement {
+        if (category == null) {
+            throw new IllegalArgumentException("The category is null!");
+        }
+
+        if (minAmountEnrolled < 0) {
+            throw new IllegalArgumentException("The credits are negative!");}
+    }
+}
