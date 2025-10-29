d7efc66c7cea0df187f85947f6dfd572e04d4f76
Irena Stancheva <147410058+ren-rens@users.noreply.github.com>
2025-10-29 15:22:22 +0200

    Create SemesterPlan.java

---
+++ seminars/sem_03/bg.sofia.uni.fmi.mjt.burnout/plan/SemesterPlan.java
@@ -0,0 +1,31 @@
+package bg.sofia.uni.fmi.mjt.burnout.plan;
+
+import bg.sofia.uni.fmi.mjt.burnout.subject.SubjectRequirement;
+import bg.sofia.uni.fmi.mjt.burnout.subject.UniversitySubject;
+
+/**
+ * Represents a requirement for the amount of subjects needed to be enrolled during the semester.
+ *
+ * @param subjects the array of all subjects that a student can enroll in a given semester
+ * @param subjectRequirements the array of requirements for the subjects enrolled for the category
+ * @param minimalAmountOfCredits minimum amount of subject enrolled for the category
+ *
+ * @throws IllegalArgumentException if the subjects array is null
+ * @throws IllegalArgumentException if the subjectRequirements array is null
+ * @throws IllegalArgumentException if the minimalAmountOfCredits is negative
+ */
+public record SemesterPlan(UniversitySubject[] subjects, SubjectRequirement[] subjectRequirements, int minimalAmountOfCredits) {
+    public SemesterPlan {
+        if (subjects == null) {
+            throw new IllegalArgumentException("The subjects array is null");
+        }
+
+        if (subjectRequirements == null) {
+            throw new IllegalArgumentException("The subjectRequirements array is null");
+        }
+
+        if (minimalAmountOfCredits < 0) {
+            throw new IllegalArgumentException("The minimalAmountOfCredits is negative");
+        }
+    }
+}
