2c30279c305e51d1db7a3e5c7c5798474079224a
Irena Stancheva <147410058+ren-rens@users.noreply.github.com>
2025-10-29 15:20:57 +0200

    Create CryToStudentsDepartmentException.java

---
+++ seminars/sem_03/bg.sofia.uni.fmi.mjt.burnout/exception/CryToStudentsDepartmentException.java
@@ -0,0 +1,11 @@
+package bg.sofia.uni.fmi.mjt.burnout.exception;
+
+public class CryToStudentsDepartmentException extends Throwable{
+    public CryToStudentsDepartmentException(String message) {
+        super(message);
+    }
+
+    public CryToStudentsDepartmentException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
