9a69c98d47c266aa123232a7b8ea1331eb59ffaf
Irena Stancheva <147410058+ren-rens@users.noreply.github.com>
2025-11-06 15:25:46 +0200

    Create WorkoutByDifficulty.java

---
+++ seminars/sem_04/bg.sofia.uni.fmi.mjt.fittrack/workout/WorkoutByDifficulty.java
@@ -0,0 +1,10 @@
+package bg.sofia.uni.fmi.mjt.fittrack.workout;
+
+import java.util.Comparator;
+
+public class WorkoutByDifficulty implements Comparator<Workout> {
+    @Override
+    public int compare(Workout o1, Workout o2) {
+        return Integer.compare(o2.getDifficulty(), o1.getDifficulty());
+    }
+}
