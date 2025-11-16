fa1bf411f05ddb5078b57ad43e2edd8e28d0c65c
Irena Stancheva <147410058+ren-rens@users.noreply.github.com>
2025-11-16 16:09:59 +0200

    Update LowAttributeSumEliminationRule.java

--- seminars/sem_02/bg.sofia.uni.fmi.mjt.show/elimination/LowAttributeSumEliminationRule.java
+++ seminars/sem_02/bg.sofia.uni.fmi.mjt.show/elimination/LowAttributeSumEliminationRule.java
@@ -2,23 +2,57 @@ package bg.sofia.uni.fmi.mjt.show.elimination;
 
 import bg.sofia.uni.fmi.mjt.show.ergenka.Ergenka;
 
-public class LowAttributeSumEliminationRule implements EliminationRule{
+public class LowAttributeSumEliminationRule implements EliminationRule {
+
     public LowAttributeSumEliminationRule(int threshold) {
         this.threshold = threshold;
     }
+
     @Override
     public Ergenka[] eliminateErgenkas(Ergenka[] ergenkas) {
-        Ergenka[] eliminateErgenkas = new Ergenka[ergenkas.length];
-        int idx = 0;
+        if (ergenkas == null || ergenkas.length == 0) {
+            return (ergenkas == null) ? new Ergenka[0] : ergenkas;
+        }
 
+        // Count how many will REMAIN (those meeting the threshold)
+        int remainingCount = countOfRemainingErgenkas(ergenkas);
+
+        // Build array of REMAINING ergenkas
+        return findRemainingErgenkas(ergenkas, remainingCount);
+    }
+
+    private int countOfRemainingErgenkas(Ergenka[] ergenkas) {
+        int remainingCount = 0;
         for (Ergenka ergenka : ergenkas) {
-            if (ergenka.getHumorLevel() + ergenka.getRomanceLevel() < this.threshold) {
-                eliminateErgenkas[idx++] = ergenka;
+            if (ergenka == null) {
+                continue;
+            }
+
+            if (ergenka.getHumorLevel() + ergenka.getRomanceLevel() >= this.threshold) {
+                remainingCount++;
             }
         }
 
-        return eliminateErgenkas;
+        return  remainingCount;
+    }
+
+    private Ergenka[] findRemainingErgenkas(Ergenka[] ergenkas, int remainingCount) {
+        Ergenka[] remaining = new Ergenka[remainingCount];
+        int idx = 0;
+
+        for (Ergenka ergenka : ergenkas) {
+            if (ergenka == null) {
+                continue;
+            }
+
+            if (ergenka.getHumorLevel() + ergenka.getRomanceLevel() >= this.threshold) {
+                remaining[idx++] = ergenka;
+            }
+        }
+
+        return remaining;
     }
 
     private int threshold;
+
 }
