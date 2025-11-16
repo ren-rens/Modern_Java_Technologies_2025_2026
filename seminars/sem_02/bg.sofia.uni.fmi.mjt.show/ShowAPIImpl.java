af34782b886535503490ba05b88f019b56e75ac9
Irena Stancheva <147410058+ren-rens@users.noreply.github.com>
2025-11-16 16:08:26 +0200

    Update ShowAPIImpl.java

--- seminars/sem_02/bg.sofia.uni.fmi.mjt.show/ShowAPIImpl.java
+++ seminars/sem_02/bg.sofia.uni.fmi.mjt.show/ShowAPIImpl.java
@@ -2,36 +2,50 @@ package bg.sofia.uni.fmi.mjt.show;
 
 import bg.sofia.uni.fmi.mjt.show.date.DateEvent;
 import bg.sofia.uni.fmi.mjt.show.elimination.EliminationRule;
+import bg.sofia.uni.fmi.mjt.show.elimination.LowestRatingEliminationRule;
 import bg.sofia.uni.fmi.mjt.show.ergenka.Ergenka;
 
-public class ShowAPIImpl implements ShowAPI{
+public class ShowAPIImpl implements ShowAPI {
+
     public ShowAPIImpl(Ergenka[] ergenkas, EliminationRule[] defaultEliminationRules) {
         this.ergenkas = ergenkas;
-        this.defaultEliminationRules = defaultEliminationRules;
+
+        if (defaultEliminationRules == null || defaultEliminationRules.length == 0) {
+            this.defaultEliminationRules = new EliminationRule[] { new LowestRatingEliminationRule() };
+        } else {
+            this.defaultEliminationRules = defaultEliminationRules;
+        }
     }
 
     @Override
     public Ergenka[] getErgenkas() {
-        if (!isValid()) {
-            return null;
-        }
         return ergenkas;
     }
 
     @Override
     public void playRound(DateEvent dateEvent) {
-        if (!isValid()) {
-            return;
+        if (ergenkas == null) {
+            return;  // Do nothing if no ergenkas
+        }
+
+        // If dateEvent is null, use a default DateEvent
+        if (dateEvent == null) {
+            //dateEvent = new DateEvent("Default", 5, 60);
+            dateEvent = new DateEvent("", 0, 0);
         }
 
         for (Ergenka ergenka : this.ergenkas) {
+            if (ergenka == null) {
+                continue;
+            }
+
             ergenka.reactToDate(dateEvent);
         }
     }
 
     @Override
     public void eliminateErgenkas(EliminationRule[] eliminationRules) {
-        if (!isValid()) {
+        if (ergenkas == null) {
             return;
         }
 
@@ -39,57 +53,37 @@ public class ShowAPIImpl implements ShowAPI{
             eliminationRules = this.defaultEliminationRules;
         }
 
+        // If still no rules to apply, return
+        if (eliminationRules == null || eliminationRules.length == 0) {
+            return;
+        }
+
+        // Each rule returns the REMAINING ergenkas after elimination
         for (EliminationRule eliminationRule : eliminationRules) {
-            Ergenka[] eliminatedErgenkas = eliminationRule.eliminateErgenkas(this.ergenkas);
-
-            int eliminatedCount = eliminatedErgenkas.length;
-            if (eliminatedCount == 0) {
+            if (eliminationRule == null) {
                 continue;
             }
 
-            int size = this.ergenkas.length;
-            if (eliminatedCount == size) {
-                System.out.println(eliminatedCount);
+            // The rule returns the remaining ergenkas directly
+            this.ergenkas = eliminationRule.eliminateErgenkas(this.ergenkas);
 
-                ergenkas = null;
-                ergenkas = new Ergenka[]{};
-                continue;
-            }
-
-            int idx = 0;
-            Ergenka[] remainingErgenkas = new Ergenka[size - eliminatedErgenkas.length];
-
-            for (Ergenka ergenka : this.ergenkas) {
-                boolean isEliminated = false;
-                for (Ergenka eliminated : eliminatedErgenkas) {
-                    if (ergenka.equals(eliminated)) {
-                        isEliminated = true;
-                        break;
-                    }
-                }
-
-                if (isEliminated) {
-                    continue;
-                }
-
-
-                remainingErgenkas[idx++] = ergenka;
-
-            }
-
-            this.ergenkas = remainingErgenkas;
+//            if (this.ergenkas == null) {
+//                this.ergenkas = new Ergenka[0];
+//            }
         }
     }
 
     @Override
     public void organizeDate(Ergenka ergenka, DateEvent dateEvent) {
+        if (ergenka == null) {
+            return;
+        }
+
         ergenka.reactToDate(dateEvent);
     }
 
-    private boolean isValid() {
-        return this.ergenkas != null && this.ergenkas.length != 0;
-    }
 
     private Ergenka[] ergenkas;
     private EliminationRule[] defaultEliminationRules;
+
 }
