7de9a456d7e3a0b5ca724b02849fc586113712c4
Irena Stancheva <147410058+ren-rens@users.noreply.github.com>
2025-11-16 16:09:34 +0200

    Update PublicVoteEliminationRule.java

--- seminars/sem_02/bg.sofia.uni.fmi.mjt.show/elimination/PublicVoteEliminationRule.java
+++ seminars/sem_02/bg.sofia.uni.fmi.mjt.show/elimination/PublicVoteEliminationRule.java
@@ -2,24 +2,50 @@ package bg.sofia.uni.fmi.mjt.show.elimination;
 
 import bg.sofia.uni.fmi.mjt.show.ergenka.Ergenka;
 
-public class PublicVoteEliminationRule implements EliminationRule{
+public class PublicVoteEliminationRule implements EliminationRule {
     public PublicVoteEliminationRule(String[] votes) {
         this.votes = votes;
     }
 
     @Override
     public Ergenka[] eliminateErgenkas(Ergenka[] ergenkas) {
-        /*
-        votesToErgenkaCount:
-        idx -> ergenkas[i] (Ergenka's name)
-        count -> the count of the votes for this Ergenka
-         */
+        if (ergenkas == null || ergenkas.length == 0 || votes == null || votes.length == 0) {
+            return (ergenkas == null) ? new Ergenka[0] : ergenkas;
+        }
 
         int ergenkasSize = ergenkas.length;
+
+        // Count votes for each ergenka
+        int[] votesToErgenkaCount = countVotesForEachErgenka(ergenkas, ergenkasSize);
+
+        // Calculate required votes for elimination (50% + 1)
+        int requiredVotes = votes.length / 2 + 1;
+
+        // Find if any ergenka has 50% + 1 votes (to be eliminated)
+        int eliminatedIndex = findIdxOfErgenkaToEliminate(votesToErgenkaCount, requiredVotes, ergenkasSize);
+
+        // If no one has 50%+1 votes, return all (no elimination)
+        if (eliminatedIndex == -1) {
+            return ergenkas;
+        }
+
+        // Build array of REMAINING ergenkas (all except the eliminated one)
+        return findRemainingErgenkas(ergenkas, ergenkasSize, eliminatedIndex);
+    }
+
+    private int[] countVotesForEachErgenka(Ergenka[] ergenkas, int ergenkasSize) {
         int[] votesToErgenkaCount = new int[ergenkasSize];
 
         for (String vote : votes) {
+            if (vote == null) {
+                continue;
+            }
+
             for (int j = 0; j < ergenkasSize; j++) {
+                if (ergenkas[j] == null) {
+                    continue;
+                }
+
                 if (vote.equals(ergenkas[j].getName())) {
                     votesToErgenkaCount[j]++;
                     break;
@@ -27,13 +53,32 @@ public class PublicVoteEliminationRule implements EliminationRule{
             }
         }
 
+        return votesToErgenkaCount;
+    }
+
+    private int findIdxOfErgenkaToEliminate(int[] votesToErgenkaCount, int requiredVotes, int ergenkasSize) {
+        int eliminatedIndex = -1;
         for (int i = 0; i < ergenkasSize; i++) {
-            if (votesToErgenkaCount[i] == 0.5 * ergenkasSize + 1) {
-                return new Ergenka[] {ergenkas[i]};
+            if (votesToErgenkaCount[i] >= requiredVotes) {
+                eliminatedIndex = i;
+                break;
             }
         }
 
-        return new Ergenka[] {};
+        return eliminatedIndex;
+    }
+
+    private Ergenka[] findRemainingErgenkas(Ergenka[] ergenkas, int ergenkasSize, int eliminatedIndex) {
+        Ergenka[] remaining = new Ergenka[ergenkasSize - 1];
+        int idx = 0;
+
+        for (int i = 0; i < ergenkasSize; i++) {
+            if (i != eliminatedIndex && ergenkas[i] != null) {
+                remaining[idx++] = ergenkas[i];
+            }
+        }
+
+        return remaining;
     }
 
     private String[] votes;
