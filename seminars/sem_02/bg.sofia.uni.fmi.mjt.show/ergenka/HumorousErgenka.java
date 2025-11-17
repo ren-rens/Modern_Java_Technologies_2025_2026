852f447b0ddd714c4d58ee8298347a526fbd656c
Irena Stancheva <147410058+ren-rens@users.noreply.github.com>
2025-11-17 21:41:34 +0200

    Refactor reactToDate and findBonuses methods

--- seminars/sem_02/bg.sofia.uni.fmi.mjt.show/ergenka/HumorousErgenka.java
+++ seminars/sem_02/bg.sofia.uni.fmi.mjt.show/ergenka/HumorousErgenka.java
@@ -3,6 +3,7 @@ package bg.sofia.uni.fmi.mjt.show.ergenka;
 import bg.sofia.uni.fmi.mjt.show.date.DateEvent;
 
 public class HumorousErgenka implements Ergenka {
+    
     public HumorousErgenka(String name, short age, int romanceLevel, int humorLevel, int rating) {
         this.name = name;
         this.age = age;
@@ -38,39 +39,37 @@ public class HumorousErgenka implements Ergenka {
 
     @Override
     public void reactToDate(DateEvent dateEvent) {
-        if (dateEvent == null) {
-            return;  // Do nothing if dateEvent is null
-        }
+        int ratingChange = 0;
 
-        int bonuses = 0;
+        if (dateEvent != null && dateEvent.getTensionLevel() != 0) {
+            int bonuses = findBonuses(dateEvent);
 
-        int duration = dateEvent.getDuration();
-        if (duration >= 30 && duration <= 90) {
-            bonuses += 4;
-        } else if (duration < 30) {
-            bonuses -= 2;
-        } else {
-            bonuses -= 3;
-        }
-
-        int dateTension = dateEvent.getTensionLevel();
-
-        // Protect against division by zero
-        int ratingChange;
-        if (dateTension == 0) {
-            //ratingChange = 0;
-            return;
-        } else {
-            ratingChange = (this.humorLevel * 5) / dateTension +
+            ratingChange = (this.humorLevel * 5) / dateEvent.getTensionLevel() +
                 Math.floorDiv(this.romanceLevel, 3) + bonuses;
         }
 
         this.rating += ratingChange;
     }
 
-    private String name;
-    private short age;
-    private int romanceLevel;
-    private int humorLevel;
+    private int findBonuses(DateEvent dateEvent) {
+        int bonuses = 0;
+        int duration = dateEvent.getDuration();
+
+        if (duration >= 30 && duration <= 90) {
+            bonuses += 4;
+        } else if (duration < 30) {
+            bonuses -= 2;
+        } else if (duration > 90) {
+            bonuses -= 3;
+        }
+
+        return bonuses;
+    }
+
+    private final String name;
+    private final short age;
+    private final int romanceLevel;
+    private final int humorLevel;
     private int rating;
+    
 }
