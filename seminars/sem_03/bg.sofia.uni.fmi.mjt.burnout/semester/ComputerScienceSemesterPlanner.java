2143b2405889e8bd37fe0dafe6243e4f658dae4b
Irena Stancheva <147410058+ren-rens@users.noreply.github.com>
2025-11-18 00:53:36 +0200

    Update ComputerScienceSemesterPlanner.java

--- seminars/sem_03/bg.sofia.uni.fmi.mjt.burnout/semester/ComputerScienceSemesterPlanner.java
+++ seminars/sem_03/bg.sofia.uni.fmi.mjt.burnout/semester/ComputerScienceSemesterPlanner.java
@@ -2,18 +2,22 @@ package bg.sofia.uni.fmi.mjt.burnout.semester;
 
 import bg.sofia.uni.fmi.mjt.burnout.exception.CryToStudentsDepartmentException;
 import bg.sofia.uni.fmi.mjt.burnout.exception.DisappointmentException;
+import bg.sofia.uni.fmi.mjt.burnout.exception.InvalidSubjectRequirementsException;
 import bg.sofia.uni.fmi.mjt.burnout.plan.SemesterPlan;
+import bg.sofia.uni.fmi.mjt.burnout.subject.Category;
+import bg.sofia.uni.fmi.mjt.burnout.subject.SubjectRequirement;
 import bg.sofia.uni.fmi.mjt.burnout.subject.UniversitySubject;
 
 public final class ComputerScienceSemesterPlanner extends AbstractSemesterPlanner {
-    
+
     @Override
-    public int calculateJarCount(UniversitySubject[] subjects, int maximumSlackTime, int semesterDuration) throws IllegalArgumentException, DisappointmentException {
+    public int calculateJarCount(UniversitySubject[] subjects, int maximumSlackTime, int semesterDuration) {
         return super.calculateJarCount(subjects, maximumSlackTime, semesterDuration);
     }
 
     @Override
-    public UniversitySubject[] calculateSubjectList(SemesterPlan semesterPlan) throws CryToStudentsDepartmentException, IllegalArgumentException{
+    public UniversitySubject[] calculateSubjectList(SemesterPlan semesterPlan)
+        throws InvalidSubjectRequirementsException {
         if (semesterPlan == null) {
             throw new IllegalArgumentException("Semester plan is null!");
         }
@@ -23,17 +27,37 @@ public final class ComputerScienceSemesterPlanner extends AbstractSemesterPlanne
             throw new IllegalArgumentException("UniversitySubjects array is null!");
         }
 
+        SubjectRequirement[] requirements = semesterPlan.subjectRequirements();
+        if (requirements == null) {
+            throw new IllegalArgumentException("Subject requirements cannot be null");
+        }
+
+        checkForDuplicateCategories(requirements);
+
         sortByRating(subjects); // the reference is copied but the place where it points changes
 
         int minimalAmountOfCredits = semesterPlan.minimalAmountOfCredits();
         UniversitySubject[] currentSubjects = new UniversitySubject[semesterPlan.subjects().length];
 
-        int size = coverSubjects(minimalAmountOfCredits, subjects, currentSubjects); // the referance is copied but the value is changed and we need the number of covered subjects so we return it
+        int size = coverSubjects(minimalAmountOfCredits, subjects,
+            currentSubjects); // the referance is copied but the value is changed and we need the number of covered subjects so we return it
 
         UniversitySubject[] res = new UniversitySubject[size];
         System.arraycopy(currentSubjects, 0, res, 0, size);
 
         return res;
+    }
+
+    private void checkForDuplicateCategories(SubjectRequirement[] requirements)
+        throws InvalidSubjectRequirementsException {
+        boolean[] seen = new boolean[Category.values().length];
+        for (SubjectRequirement req : requirements) {
+            if (seen[req.category().getIdx()]) {
+                throw new InvalidSubjectRequirementsException(
+                    "The subjectRequirements contain duplicate categories");
+            }
+            seen[req.category().getIdx()] = true;
+        }
     }
 
     private void sortByRating(UniversitySubject[] subjects) {
@@ -61,7 +85,8 @@ public final class ComputerScienceSemesterPlanner extends AbstractSemesterPlanne
         }
     }
 
-    private int coverSubjects(int minimalAmountOfCredits, UniversitySubject[] subjects, UniversitySubject[] currentSubjects) throws CryToStudentsDepartmentException{
+    private int coverSubjects(int minimalAmountOfCredits, UniversitySubject[] subjects,
+                              UniversitySubject[] currentSubjects) throws CryToStudentsDepartmentException {
         int idx = 0;
         int currCredits = 0;
 
@@ -80,5 +105,5 @@ public final class ComputerScienceSemesterPlanner extends AbstractSemesterPlanne
 
         return idx;
     }
-    
+
 }
