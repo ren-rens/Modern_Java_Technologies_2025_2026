f65e6a43b845ef1936b27c8007125490073ab48e
Irena Stancheva <147410058+ren-rens@users.noreply.github.com>
2025-11-18 00:53:04 +0200

    Update SemesterPlan.java

--- seminars/sem_03/bg.sofia.uni.fmi.mjt.burnout/plan/SemesterPlan.java
+++ seminars/sem_03/bg.sofia.uni.fmi.mjt.burnout/plan/SemesterPlan.java
@@ -6,16 +6,17 @@ import bg.sofia.uni.fmi.mjt.burnout.subject.UniversitySubject;
 /**
  * Represents a requirement for the amount of subjects needed to be enrolled during the semester.
  *
- * @param subjects the array of all subjects that a student can enroll in a given semester
- * @param subjectRequirements the array of requirements for the subjects enrolled for the category
+ * @param subjects               the array of all subjects that a student can enroll in a given semester
+ * @param subjectRequirements    the array of requirements for the subjects enrolled for the category
  * @param minimalAmountOfCredits minimum amount of subject enrolled for the category
- *
  * @throws IllegalArgumentException if the subjects array is null
  * @throws IllegalArgumentException if the subjectRequirements array is null
  * @throws IllegalArgumentException if the minimalAmountOfCredits is negative
  */
-public record SemesterPlan(UniversitySubject[] subjects, SubjectRequirement[] subjectRequirements, int minimalAmountOfCredits) {
-    
+@SuppressWarnings("checkstyle:LineLength")
+public record SemesterPlan(UniversitySubject[] subjects, SubjectRequirement[] subjectRequirements,
+                           int minimalAmountOfCredits) {
+
     public SemesterPlan {
         if (subjects == null) {
             throw new IllegalArgumentException("The subjects array is null");
@@ -29,5 +30,5 @@ public record SemesterPlan(UniversitySubject[] subjects, SubjectRequirement[] su
             throw new IllegalArgumentException("The minimalAmountOfCredits is negative");
         }
     }
-    
+
 }
