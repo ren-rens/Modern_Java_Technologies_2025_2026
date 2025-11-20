a435d9e79af0791a3a44ea318edbc0b890c354d4
Irena Stancheva <147410058+ren-rens@users.noreply.github.com>
2025-11-20 14:43:23 +0200

    Update FileTest.java

--- seminars/sem_06/test/bg.sofia.uni.fmi.mjt/file/FileTest.java
+++ seminars/sem_06/test/bg.sofia.uni.fmi.mjt/file/FileTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class FileTest {
+class FileTest {
 
     @Test
     void testSetContentWithNull() {
