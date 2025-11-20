0337983f031a9adf24e8fc59a8e620f7d7e222ff
Irena Stancheva <147410058+ren-rens@users.noreply.github.com>
2025-11-20 14:40:18 +0200

    Create FileTest.java

---
+++ seminars/sem_06/test/bg.sofia.uni.fmi.mjt/file/FileTest.java
@@ -0,0 +1,17 @@
+package bg.sofia.uni.fmi.mjt.file;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FileTest {
+
+    @Test
+    void testSetContentWithNull() {
+        File file = new File("file.txt");
+        assertThrows(IllegalArgumentException.class, () -> file.setContent(null),
+            "When setting file content with null should throw IllegalArgumentException");
+    }
+
+}
