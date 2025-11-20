ce5639cbb96b3befd4a82ba78cdddedf594ffa96
Irena Stancheva <147410058+ren-rens@users.noreply.github.com>
2025-11-20 14:37:54 +0200

    Create UpperCaseFile.java

---
+++ seminars/sem_06/src/bg.sofia.uni.fmi.mjt/file/step/UpperCaseFile.java
@@ -0,0 +1,31 @@
+package bg.sofia.uni.fmi.mjt.file.step;
+
+import bg.sofia.uni.fmi.mjt.file.File;
+import bg.sofia.uni.fmi.mjt.pipeline.step.Step;
+
+/**
+ * A pipeline step that transforms the content of a {@link File} to uppercase.
+ * <p>
+ * This step modifies the {@link File} in place by converting its content
+ * to uppercase.
+ */
+public class UpperCaseFile implements Step<File, File> {
+
+    /**
+     * Converts the content of the given {@link File} to uppercase.
+     *
+     * @param input the file whose content will be transformed
+     * @return the same {@link File} object with updated content
+     * @throws IllegalArgumentException if the input file or its content is null
+     */
+    @Override
+    public File process(File input) {
+        if (input == null || input.getContent() == null || input.getContent().isEmpty()) {
+            throw new IllegalArgumentException("Invalid file input given to UpperCaseFile.process!");
+        }
+
+        input = new File(input.getContent().toUpperCase());
+        return input;
+    }
+
+}
