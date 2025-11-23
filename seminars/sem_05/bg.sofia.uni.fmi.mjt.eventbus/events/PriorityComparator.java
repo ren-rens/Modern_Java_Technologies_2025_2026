61928a1f4e7c103f33bd841fcd2e85402a6501b9
Irena Stancheva <147410058+ren-rens@users.noreply.github.com>
2025-11-23 15:43:09 +0200

    Update PriorityComparator.java

--- seminars/sem_05/bg.sofia.uni.fmi.mjt.eventbus/events/PriorityComparator.java
+++ seminars/sem_05/bg.sofia.uni.fmi.mjt.eventbus/events/PriorityComparator.java
@@ -6,7 +6,9 @@ public class PriorityComparator<T extends Event<?>> implements Comparator<T> {
 
     @Override
     public int compare(Event o1, Event o2) {
-        return Integer.compare(o1.getPriority(), o2.getPriority());
+        int priorityComparison = Integer.compare(o1.getPriority(), o2.getPriority());
+
+        return priorityComparison != 0 ? priorityComparison : o1.getTimestamp().compareTo(o2.getTimestamp());
     }
 
 }
